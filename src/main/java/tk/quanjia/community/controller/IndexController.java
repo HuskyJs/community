78e11cfc9dc8d76b5cfc1216924643bc458e0cbc
HuskyJs <61821041+HuskyJs@users.noreply.github.com>
2022-06-26 18:23:17 +0800

    add index and bootstraps

--- src/main/java/tk/quanjia/community/controller/IndexController.java
+++ src/main/java/tk/quanjia/community/controller/IndexController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller //我允许当前的类接收前端请求
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name",required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")    //  "/"代表默认启动模板
+    public String index() {
+        return "index";
     }
 }
